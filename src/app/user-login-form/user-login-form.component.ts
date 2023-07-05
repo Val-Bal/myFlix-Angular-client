05c41c77da2cb5d68aa5836da8d7abcd9b964a65
Valeria Balducci <v_balducci@web.de>
2023-07-05 09:42:31 +0200

    change import class and define type of result

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
 // This import brings in the API calls we created in 6.2
-import { FetchApiDataService } from '../fetch-api-data.service';
+import { UserRegistrationService } from '../fetch-api-data.service';
 
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -21,7 +21,7 @@ export class UserLoginFormComponent implements OnInit {
   @Input() loginData = { Username: '', Password: ''};
 
 constructor(
-    public fetchApiData: FetchApiDataService,
+    public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar) { }
 
@@ -30,7 +30,7 @@ ngOnInit(): void {
 
 // This is the function responsible for sending the form inputs to the backend
 loginUser(): void {
-    this.fetchApiData.userLogin(this.loginData).subscribe((result) => {
+    this.fetchApiData.userLogin(this.loginData).subscribe((result: any) => {
   // Logic for a successful user login goes here! (To be implemented)
      console.log(result);
      localStorage.setItem('username', result.user.Username);
